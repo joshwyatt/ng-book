fb6af39c5d8021ceea37a5dce6143df4131e93c7
joshWyatt <joshuawyatt123@gmail.com>
2014-10-08 16:44:15 -0700

    (feat) Make hello world app / (feat) Implement basic static file handling / (feat) Create lib and install angular

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static(__dirname + '../'));
+app.use(express.static(__dirname + '/..'));
+
 
 app.listen(8888);
+console.log('listening on 8888');
